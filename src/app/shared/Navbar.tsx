362623afcfa97f8c1a2668006377c84888745027
Badhon-Roy <roybadhon286@gmail.com>
2025-09-25 10:53:25 +0600

    Basic setup done and navbar created

---
+++ src/app/shared/Navbar.tsx
@@ -0,0 +1,13 @@
+import Logo from "@/app/assets/logo.png"
+import Image from "next/image";
+
+const Navbar = () => {
+    return (
+        <div className="flex items-center justify-between py-5 lg:px-0 px-4 max-w-[1200px] mx-auto">
+            <Image src={Logo} alt="Logo" width={120} height={40} />
+            <button className="lg:w-[142px] w-[120px] h-[45px] bg-[#3BA334] rounded-lg text-white drop-shadow-sm drop-shadow-[#3BA334]">Get Started</button>
+        </div>
+    );
+};
+
+export default Navbar;
